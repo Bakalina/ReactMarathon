657539c1543b9db6ae49fef25824314edf5c8a75
Yaroslav <bakalyna.yaroslav@gmail.com>
2022-12-09 23:42:26 +0200

    task02

---
+++ Sprint01/task02-jsx/src/components/my-input/my-input.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MyInput = () => {
+    return <input id='inp-num' type='number' data-testid='element-input'/>
+}
+
+export default MyInput;
---
+++ Sprint01/task02-jsx/src/components/my-label/my-label.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const MyLabel = () => {
+    return <label htmlFor='inp-num' data-testid='element-label'>
+        Count:
+    </label>
+}
+
+export default MyLabel;
